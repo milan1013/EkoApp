47e9192654d8bf8474931981c8a96a6a02cfe723
Martin <bonderovicmartin8@gmail.com>
2022-11-05 16:07:48 +0100

    main - Implement router and navbar

--- ecosity/src/App.js
+++ ecosity/src/App.js
@@ -1,9 +1,23 @@
+import Navbar from "./Components/Navbar/Navbar";
+import "./Components/Navbar/Navbar.css";
+import Profile from "./Pages/Profile";
+import Map from "./Pages/Map";
+import Home from "./Pages/Home";
+import MyEvents from "./Pages/MyEvents";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/myevents" element={<MyEvents />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
